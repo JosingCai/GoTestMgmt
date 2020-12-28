37d4c418cd6ba7f2a17d2969a64b2c700cb7c71a
Josing <caijiaoxing@idcos.com>
2020-12-28 19:45:25 +0800

    Add dashboard info

--- pages/dashboard.go
+++ pages/dashboard.go
@@ -1,63 +1,113 @@
 package pages
 
 import (
+	"html/template"
+
 	tmpl "github.com/GoAdminGroup/go-admin/template"
 	"github.com/GoAdminGroup/go-admin/template/chartjs"
 	"github.com/GoAdminGroup/go-admin/template/icon"
 	"github.com/GoAdminGroup/go-admin/template/types"
-	"github.com/GoAdminGroup/themes/adminlte/components/chart_legend"
-	"github.com/GoAdminGroup/themes/adminlte/components/description"
 	"github.com/GoAdminGroup/themes/adminlte/components/infobox"
-	"github.com/GoAdminGroup/themes/adminlte/components/productlist"
-	"github.com/GoAdminGroup/themes/adminlte/components/progress_group"
 	"github.com/GoAdminGroup/themes/adminlte/components/smallbox"
+	"github.com/GoAdminGroup/themes/sword/components/chart_legend"
 	"github.com/gin-gonic/gin"
-	"html/template"
 )
+
+type Box struct {
+	Title  template.HTML
+	Color  template.HTML
+	Number template.HTML
+	Icon   template.HTML
+}
 
 func GetDashBoardContent(ctx *gin.Context) (types.Panel, error) {
 
 	components := tmpl.Default()
 	colComp := components.Col()
+	var size = types.SizeMD(3).SM(6).XS(12)
+	/**************************
+	 * Small Box
+	/**************************/
 
+	smallbox1 := smallbox.New().SetColor("yellow").SetIcon("fa-eye").SetUrl("/admin/info/api_case").SetTitle("api").SetValue("接口用例").GetContent()
+	smallbox2 := smallbox.New().SetColor("red").SetIcon("fa-eye-slash").SetUrl("/admin/info/test_case").SetTitle("case").SetValue("测试用例").GetContent()
+	smallbox3 := smallbox.New().SetColor("green").SetIcon("fa-fast-backward").SetUrl("/admin/info/api_sum_up").SetTitle("api count").SetValue("接口总览").GetContent()
+	smallbox4 := smallbox.New().SetColor("purple").SetIcon("fa-fast-forward").SetUrl("/admin/info/testcase_count").SetTitle("case count").SetValue("用例统计").GetContent()
+
+	col1 := colComp.SetSize(size).SetContent(smallbox1).GetContent()
+	col2 := colComp.SetSize(size).SetContent(smallbox2).GetContent()
+	col3 := colComp.SetSize(size).SetContent(smallbox3).GetContent()
+	col4 := colComp.SetSize(size).SetContent(smallbox4).GetContent()
+
+	row1 := components.Row().SetContent(col1 + col2 + col3 + col4).GetContent()
 	/**************************
 	 * Info Box
 	/**************************/
-
+	box1 := Box{"CPU TRAFFIC", "aqua", "100", "ion-ios-gear-outline"}
+	box2 := Box{"Likes", "red", "1030.00<small>$</small>", icon.GooglePlus}
+	box3 := Box{"Sales", "green", "760", "ion-ios-cart-outline"}
+	box4 := Box{"New Members", "yellow", "2,349", "ion-ios-people-outline"}
 	infobox1 := infobox.New().
-		SetText("CPU TRAFFIC").
-		SetColor("aqua").
-		SetNumber("100").
-		SetIcon("ion-ios-gear-outline").
+		SetText(box1.Title).
+		SetColor(box1.Color).
+		SetNumber(box1.Number).
+		SetIcon(box1.Icon).
 		GetContent()
 
 	infobox2 := infobox.New().
-		SetText("Likes").
-		SetColor("red").
-		SetNumber("1030.00<small>$</small>").
-		SetIcon(icon.GooglePlus).
+		SetText(box2.Title).
+		SetColor(box2.Color).
+		SetNumber(box2.Number).
+		SetIcon(box2.Icon).
 		GetContent()
 
 	infobox3 := infobox.New().
-		SetText("Sales").
-		SetColor("green").
-		SetNumber("760").
-		SetIcon("ion-ios-cart-outline").
+		SetText(box3.Title).
+		SetColor(box3.Color).
+		SetNumber(box3.Number).
+		SetIcon(box3.Icon).
 		GetContent()
 
 	infobox4 := infobox.New().
-		SetText("New Members").
-		SetColor("yellow").
-		SetNumber("2,349").
-		SetIcon("ion-ios-people-outline"). // svg is ok
+		SetText(box4.Title).
+		SetColor(box4.Color).
+		SetNumber(box4.Number).
+		SetIcon(box4.Icon).
+		GetContent()
+	infobox5 := infobox.New().
+		SetText(box4.Title).
+		SetColor(box4.Color).
+		SetNumber(box4.Number).
+		SetIcon(box4.Icon).
+		GetContent()
+	infobox6 := infobox.New().
+		SetText(box4.Title).
+		SetColor(box4.Color).
+		SetNumber(box4.Number).
+		SetIcon(box4.Icon).
+		GetContent()
+	infobox7 := infobox.New().
+		SetText(box4.Title).
+		SetColor(box4.Color).
+		SetNumber(box4.Number).
+		SetIcon(box4.Icon).
+		GetContent()
+	infobox8 := infobox.New().
+		SetText(box4.Title).
+		SetColor(box4.Color).
+		SetNumber(box4.Number).
+		SetIcon(box4.Icon).
 		GetContent()
 
-	var size = types.SizeMD(3).SM(6).XS(12)
 	infoboxCol1 := colComp.SetSize(size).SetContent(infobox1).GetContent()
 	infoboxCol2 := colComp.SetSize(size).SetContent(infobox2).GetContent()
 	infoboxCol3 := colComp.SetSize(size).SetContent(infobox3).GetContent()
 	infoboxCol4 := colComp.SetSize(size).SetContent(infobox4).GetContent()
-	row1 := components.Row().SetContent(infoboxCol1 + infoboxCol2 + infoboxCol3 + infoboxCol4).GetContent()
+	infoboxCol5 := colComp.SetSize(size).SetContent(infobox5).GetContent()
+	infoboxCol6 := colComp.SetSize(size).SetContent(infobox6).GetContent()
+	infoboxCol7 := colComp.SetSize(size).SetContent(infobox7).GetContent()
+	infoboxCol8 := colComp.SetSize(size).SetContent(infobox8).GetContent()
+	row2 := components.Row().SetContent(infoboxCol1 + infoboxCol2 + infoboxCol3 + infoboxCol4 + infoboxCol5 + infoboxCol6 + infoboxCol7 + infoboxCol8).GetContent()
 
 	/**************************
 	 * Box
@@ -90,193 +140,17 @@ func GetDashBoardContent(ctx *gin.Context) (types.Panel, error) {
 		{Head: "Item"},
 		{Head: "Status"},
 		{Head: "Popularity"},
+		{Head: "Popularity"},
+		{Head: "Popularity"},
+		{Head: "Popularity"},
 	}).GetContent()
 
-	boxInfo := components.Box().
+	row3 := components.Box().
 		WithHeadBorder().
 		SetHeader("Latest Orders").
 		SetHeadColor("#f7f7f7").
 		SetBody(table).
-		SetFooter(`<div class="clearfix"><a href="javascript:void(0)" class="btn btn-sm btn-info btn-flat pull-left">处理订单</a><a href="javascript:void(0)" class="btn btn-sm btn-default btn-flat pull-right">查看所有新订单</a> </div>`).
 		GetContent()
-
-	tableCol := colComp.SetSize(types.SizeMD(8)).SetContent(row1 + boxInfo).GetContent()
-
-	/**************************
-	 * Product List
-	/**************************/
-
-	productList := productlist.New().SetData([]map[string]string{
-		{
-			"img":         "//adminlte.io/themes/AdminLTE/dist/img/default-50x50.gif",
-			"title":       "GoAdmin",
-			"has_tabel":   "true",
-			"labeltype":   "warning",
-			"label":       "free",
-			"description": `a framework help you build the dataviz system`,
-		}, {
-			"img":         "//adminlte.io/themes/AdminLTE/dist/img/default-50x50.gif",
-			"title":       "GoAdmin",
-			"has_tabel":   "true",
-			"labeltype":   "warning",
-			"label":       "free",
-			"description": `a framework help you build the dataviz system`,
-		}, {
-			"img":         "//adminlte.io/themes/AdminLTE/dist/img/default-50x50.gif",
-			"title":       "GoAdmin",
-			"has_tabel":   "true",
-			"labeltype":   "warning",
-			"label":       "free",
-			"description": `a framework help you build the dataviz system`,
-		}, {
-			"img":         "//adminlte.io/themes/AdminLTE/dist/img/default-50x50.gif",
-			"title":       "GoAdmin",
-			"has_tabel":   "true",
-			"labeltype":   "warning",
-			"label":       "free",
-			"description": `a framework help you build the dataviz system`,
-		},
-	}).GetContent()
-
-	boxWarning := components.Box().SetTheme("warning").WithHeadBorder().SetHeader("Recently Added Products").
-		SetBody(productList).
-		SetFooter(`<a href="javascript:void(0)" class="uppercase">View All Products</a>`).
-		GetContent()
-
-	newsCol := colComp.SetSize(types.SizeMD(4)).SetContent(boxWarning).GetContent()
-
-	row5 := components.Row().SetContent(tableCol + newsCol).GetContent()
-
-	/**************************
-	 * Box
-	/**************************/
-
-	line := chartjs.Line()
-
-	lineChart := line.
-		SetID("salechart").
-		SetHeight(180).
-		SetTitle("Sales: 1 Jan, 2019 - 30 Jul, 2019").
-		SetLabels([]string{"January", "February", "March", "April", "May", "June", "July"}).
-		AddDataSet("Electronics").
-		DSData([]float64{65, 59, 80, 81, 56, 55, 40}).
-		DSFill(false).
-		DSBorderColor("rgb(210, 214, 222)").
-		DSLineTension(0.1).
-		AddDataSet("Digital Goods").
-		DSData([]float64{28, 48, 40, 19, 86, 27, 90}).
-		DSFill(false).
-		DSBorderColor("rgba(60,141,188,1)").
-		DSLineTension(0.1).
-		GetContent()
-
-	title := `<p class="text-center"><strong>Goal Completion</strong></p>`
-	progressGroup := progress_group.New().
-		SetTitle("Add Products to Cart").
-		SetColor("#76b2d4").
-		SetDenominator(200).
-		SetMolecular(160).
-		SetPercent(80).
-		GetContent()
-
-	progressGroup1 := progress_group.New().
-		SetTitle("Complete Purchase").
-		SetColor("#f17c6e").
-		SetDenominator(400).
-		SetMolecular(310).
-		SetPercent(80).
-		GetContent()
-
-	progressGroup2 := progress_group.New().
-		SetTitle("Visit Premium Page").
-		SetColor("#ace0ae").
-		SetDenominator(800).
-		SetMolecular(490).
-		SetPercent(80).
-		GetContent()
-
-	progressGroup3 := progress_group.New().
-		SetTitle("Send Inquiries").
-		SetColor("#fdd698").
-		SetDenominator(500).
-		SetMolecular(250).
-		SetPercent(50).
-		GetContent()
-
-	boxInternalCol1 := colComp.SetContent(lineChart).SetSize(types.SizeMD(8)).GetContent()
-	boxInternalCol2 := colComp.
-		SetContent(template.HTML(title) + progressGroup + progressGroup1 + progressGroup2 + progressGroup3).
-		SetSize(types.SizeMD(4)).
-		GetContent()
-
-	boxInternalRow := components.Row().SetContent(boxInternalCol1 + boxInternalCol2).GetContent()
-
-	description1 := description.New().
-		SetPercent("17").
-		SetNumber("¥140,100").
-		SetTitle("TOTAL REVENUE").
-		SetArrow("up").
-		SetColor("green").
-		SetBorder("right").
-		GetContent()
-
-	description2 := description.New().
-		SetPercent("2").
-		SetNumber("440,560").
-		SetTitle("TOTAL REVENUE").
-		SetArrow("down").
-		SetColor("red").
-		SetBorder("right").
-		GetContent()
-
-	description3 := description.New().
-		SetPercent("12").
-		SetNumber("¥140,050").
-		SetTitle("TOTAL REVENUE").
-		SetArrow("up").
-		SetColor("green").
-		SetBorder("right").
-		GetContent()
-
-	description4 := description.New().
-		SetPercent("1").
-		SetNumber("30943").
-		SetTitle("TOTAL REVENUE").
-		SetArrow("up").
-		SetColor("green").
-		GetContent()
-
-	size2 := types.SizeSM(3).XS(6)
-	boxInternalCol3 := colComp.SetContent(description1).SetSize(size2).GetContent()
-	boxInternalCol4 := colComp.SetContent(description2).SetSize(size2).GetContent()
-	boxInternalCol5 := colComp.SetContent(description3).SetSize(size2).GetContent()
-	boxInternalCol6 := colComp.SetContent(description4).SetSize(size2).GetContent()
-
-	boxInternalRow2 := components.Row().SetContent(boxInternalCol3 + boxInternalCol4 + boxInternalCol5 + boxInternalCol6).GetContent()
-
-	box := components.Box().WithHeadBorder().SetHeader("Monthly Recap Report").
-		SetBody(boxInternalRow).
-		SetFooter(boxInternalRow2).
-		GetContent()
-
-	boxcol := colComp.SetContent(box).SetSize(types.SizeMD(12)).GetContent()
-	row2 := components.Row().SetContent(boxcol).GetContent()
-
-	/**************************
-	 * Small Box
-	/**************************/
-
-	smallbox1 := smallbox.New().SetColor("blue").SetIcon("ion-ios-gear-outline").SetUrl("/").SetTitle("new users").SetValue("345￥").GetContent()
-	smallbox2 := smallbox.New().SetColor("yellow").SetIcon("ion-ios-cart-outline").SetUrl("/").SetTitle("new users").SetValue("80%").GetContent()
-	smallbox3 := smallbox.New().SetColor("red").SetIcon("fa-user").SetUrl("/").SetTitle("new users").SetValue("645￥").GetContent()
-	smallbox4 := smallbox.New().SetColor("green").SetIcon("ion-ios-cart-outline").SetUrl("/").SetTitle("new users").SetValue("889￥").GetContent()
-
-	col1 := colComp.SetSize(size).SetContent(smallbox1).GetContent()
-	col2 := colComp.SetSize(size).SetContent(smallbox2).GetContent()
-	col3 := colComp.SetSize(size).SetContent(smallbox3).GetContent()
-	col4 := colComp.SetSize(size).SetContent(smallbox4).GetContent()
-
-	row3 := components.Row().SetContent(col1 + col2 + col3 + col4).GetContent()
 
 	/**************************
 	 * Pie Chart
@@ -315,80 +189,51 @@ func GetDashBoardContent(ctx *gin.Context) (types.Panel, error) {
 		},
 	}).GetContent()
 
-	boxDanger := components.Box().SetTheme("danger").WithHeadBorder().SetHeader("Browser Usage").
+	boxDanger := components.Box().SetTheme("danger").WithHeadBorder().SetHeader("Project").
 		SetBody(components.Row().
-			SetContent(colComp.SetSize(types.SizeMD(8)).
+			SetContent(colComp.SetSize(types.SizeMD(4)).
 				SetContent(pie).
 				GetContent() + colComp.SetSize(types.SizeMD(4)).
 				SetContent(legend).
 				GetContent()).GetContent()).
-		SetFooter(`<p class="text-center"><a href="javascript:void(0)" class="uppercase">View All Users</a></p>`).
 		GetContent()
+	row4 := components.Row().SetContent(boxDanger).GetContent()
+	// row6 := components.Row().SetContent(boxcol).GetContent()
 
 	tabs := components.Tabs().SetData([]map[string]template.HTML{
 		{
-			"title": "tabs1",
-			"content": template.HTML(`<b>How to use:</b>
-
-                <p>Exactly like the original bootstrap tabs except you should use
-                  the custom wrapper <code>.nav-tabs-custom</code> to achieve this style.</p>
-                A wonderful serenity has taken possession of my entire soul,
-                like these sweet mornings of spring which I enjoy with my whole heart.
-                I am alone, and feel the charm of existence in this spot,
-                which was created for the bliss of souls like mine. I am so happy,
-                my dear friend, so absorbed in the exquisite sense of mere tranquil existence,
-                that I neglect my talents. I should be incapable of drawing a single stroke
-                at the present moment; and yet I feel that I never was a greater artist than now.`),
+			"title": "环境配置说明",
+			"content": template.HTML(`<b>使用示例：:</b>
+                <p>1. 新建测试环境TEST</p>
+                <p>2. 在文件管理Swagger子菜单：导入API文件(TEST.json), 导入测试用例文件（TEST.xmind）</p>
+                <p>3. 选中环境，点击Swagger导入API用例，在用例菜单下即可看到对应API数据</p>
+                <p>4. 选中环境，点击Xmind导入测试用例，在用例菜单下即可看到对应测试用例数据</p>
+                <p>5. 选中环境, 点击接口统计，在报表菜单下即可看到接口统计数据</p>
+                <p>6. 选中环境, 点击用例统计，在报表菜单下即可看到用例统计数据</p>
+                <p>7. 选中环境, 点击接口总览，在报表菜单下即可看到总个项目的接口统考数据</p>
+                `),
 		}, {
-			"title": "tabs2",
+			"title": "接口用例说明",
 			"content": template.HTML(`
-                The European languages are members of the same family. Their separate existence is a myth.
-                For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ
-                in their grammar, their pronunciation and their most common words. Everyone realizes why a
-                new common language would be desirable: one could refuse to pay expensive translators. To
-                achieve this, it would be necessary to have uniform grammar, pronunciation and more common
-                words. If several languages coalesce, the grammar of the resulting language is more simple
-                and regular than that of the individual languages.
+                <p>1. 在环境设置abnomal模式，其他相关信息，API进行随机测试</p>
+                <p>2. 在测试数据菜单下准备完整的body或urlquery，方便API进行回归测试</p>
+                <p>3. 在公用变量定义API需要的各项数据，API进行组合数据测试</p>
               `),
 		}, {
-			"title": "tabs3",
+			"title": "测试用例说明",
 			"content": template.HTML(`
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                It has survived not only five centuries, but also the leap into electronic typesetting,
-                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-                like Aldus PageMaker including versions of Lorem Ipsum.
+                <p>1. 给测试用例绑定接口用例CaseID</p>
+                <p>2. 选择用例，点击回归测试，会执行测试数据下相同CaseID的全部数据</p>
+                <p>3. 选择用例，点击随机测试，会根据环境的测试模式，进行随机测试</p>
               `),
 		},
 	}).GetContent()
 
-	buttonTest := `<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">Open modal for @mdo</button>`
-	popupForm := `<form>
-          <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Recipient:</label>
-            <input type="text" class="form-control" id="recipient-name">
-          </div>
-          <div class="form-group">
-            <label for="message-text" class="col-form-label">Message:</label>
-            <textarea class="form-control" id="message-text"></textarea>
-          </div>
-        </form>`
-	popup := components.Popup().SetID("exampleModal").
-		SetFooter("Save Change").
-		SetTitle("this is a popup").
-		SetBody(template.HTML(popupForm)).
-		GetContent()
-
-	col5 := colComp.SetSize(types.SizeMD(8)).SetContent(tabs + template.HTML(buttonTest)).GetContent()
-	col6 := colComp.SetSize(types.SizeMD(4)).SetContent(boxDanger + popup).GetContent()
-
-	row4 := components.Row().SetContent(col5 + col6).GetContent()
+	row5 := components.Row().SetContent(tabs).GetContent()
 
 	return types.Panel{
-		Content:     row3 + row2 + row5 + row4,
+		Content:     row1 + row3 + row2 + row4 + row5,
 		Title:       "Dashboard",
-		Description: "dashboard example",
+		Description: "",
 	}, nil
 }
